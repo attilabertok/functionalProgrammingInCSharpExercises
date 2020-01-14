5ffbeaa0ca9cba583b4f40ff17b5fd67aff4a352
attila.bertok <hun.azazel@gmail.com>
2020-01-14 19:03:06 +0100

    Chapter 4, Exercise 1: Implement Map for ISet<T> and IDictionary<K, T>.

---
+++ FunctionalProgrammingBookExercises/Chapter4.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FunctionalProgrammingBookExercises
+{
+    public static class Chapter4
+    {
+        public static ISet<TResult> Map<TSource, TResult>(this ISet<TSource> source, Func<TSource, TResult> function)
+        {
+            var result = new HashSet<TResult>(source.Count);
+            foreach (var item in source)
+            {
+                result.Add(function(item));
+            }
+
+            return result;
+        }
+
+        public static IDictionary<TResultKey, TResultValue> Map<TSourceKey, TSourceValue, TResultKey, TResultValue>(
+            this IDictionary<TSourceKey, TSourceValue> source,
+            Func<KeyValuePair<TSourceKey, TSourceValue>, KeyValuePair<TResultKey, TResultValue>> function)
+        {
+            var result = new Dictionary<TResultKey, TResultValue>(source.Count);
+            foreach (var item in source)
+            {
+                var (key, value) = function(item);
+                result.Add(key, value);
+            }
+
+            return result;
+        }
+
+        public static IDictionary<TResultKey, TResultValue> Map<TSourceKey, TSourceValue, TResultKey, TResultValue>(
+            this IDictionary<TSourceKey, TSourceValue> source,
+            Func<TSourceKey, TResultKey> keyFunction,
+            Func<TSourceValue, TResultValue> valueFunction)
+        {
+            var function = new Func<KeyValuePair<TSourceKey, TSourceValue>, KeyValuePair<TResultKey, TResultValue>>(
+                item => new KeyValuePair<TResultKey, TResultValue>(keyFunction(item.Key), valueFunction(item.Value)));
+
+            return source.Map(function);
+        }
+    }
+}
---
+++ FunctionalProgrammingBookExercisesTests/Chapter4Tests.SetMap.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+using FluentAssertions;
+
+using Xunit;
+
+using static FunctionalProgrammingBookExercises.Chapter4;
+
+namespace FunctionalProgrammingBookExercisesTests
+{
+    public partial class Chapter4Tests
+    {
+        public class SetMap
+        {
+            [Fact]
+            public void Should_KeepItemsIntact_When_FunctionIsIdentity()
+            {
+                var originalSet = new HashSet<int> { 1, 2, 3 };
+
+                var result = originalSet.Map(i => i);
+
+                result.Should().BeEquivalentTo(originalSet);
+            }
+
+            [Fact]
+            public void Should_MapAsExpected_When_FunctionLeavesTypeIntact()
+            {
+                var originalSet = new HashSet<int> { 1, 2, 3 };
+                var expectedResult = new HashSet<int> { 1 * 2, 2 * 2, 3 * 2 };
+
+                var result = originalSet.Map(i => i * 2);
+
+                result.Should().BeEquivalentTo(expectedResult);
+            }
+
+            [Fact]
+            public void Should_MapAsExpected_When_FunctionChangesType()
+            {
+                var originalSet = new HashSet<int> { 1, 2, 3 };
+                var expectedResult = new HashSet<string> { 1.ToString(), 2.ToString(), 3.ToString() };
+
+                var result = originalSet.Map(i => i.ToString());
+
+                result.Should().BeEquivalentTo(expectedResult);
+            }
+        }
+    }
+}
